00c6a2f848c5bc80a0129f27965b2f8db34c84f1
Analy Samara Silva Santos <asss5@palmares.ifpe.edu.br>
2026-09-23 08:47:24 -0300

    feat(ui): cria placeholder da tela de calculadora

---
+++ app/src/main/java/br/edu/ifpe/ateliedasuuh/ui/features/calculadora/CalculadoraScreen.kt
@@ -0,0 +1,20 @@
+package br.edu.ifpe.ateliedasuuh.ui.features.calculadora
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CalculadoraScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Calculadora (em construção)")
+    }
+}
